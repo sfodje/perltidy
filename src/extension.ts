a9ce5b5584b8a6176968dffb457dc39aa28d7961
Chris Merry <cm@holograph.digital>
2020-01-12 10:31:28 +0000

    added new config property to support multiple language identifiers eg. perl+mojolicious

--- src/extension.ts
+++ src/extension.ts
@@ -7,22 +7,27 @@ export function activate(context: vscode.ExtensionContext) {
 
     let config = vscode.workspace.getConfiguration('perltidy');
     let formatter = new Formatter(config);
+    let languageIdentifiers: string[] = config.get('languageIdentifiers');
 
     vscode.workspace.onWillSaveTextDocument((event: vscode.TextDocumentWillSaveEvent) => {
-        if (event.document.languageId !== 'perl') return;
+        if (
+            languageIdentifiers.indexOf( event.document.languageId ) == -1
+        ) return;
         let stdFormatConfig = vscode.workspace.getConfiguration('editor');
         if (stdFormatConfig && stdFormatConfig['formatOnSave']) {
             event.waitUntil(vscode.commands.executeCommand('editor.action.formatDocument'));
         }
     });
 
-    let provider = vscode.languages.registerDocumentRangeFormattingEditProvider('perl', {
-        provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range): Promise<vscode.TextEdit[]> {
-            return formatter.format(document, range);
-        }
+    languageIdentifiers.forEach( languageIdentifier => {
+        let provider = vscode.languages.registerDocumentRangeFormattingEditProvider(languageIdentifier, {
+            provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range): Promise<vscode.TextEdit[]> {
+                return formatter.format(document, range);
+            }
+        });
+    
+        context.subscriptions.push(provider);
     });
-
-    context.subscriptions.push(provider);
 }
 
 // this method is called when your extension is deactivated
